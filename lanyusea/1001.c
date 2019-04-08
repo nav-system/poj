cad2c04d6ab3bb8e5a2d134077e92e2e55a7cdab
Phoenix <lanyusea@gmail.com>
2019-04-08 21:21:04 +0800

    NEW: shift and add functions for char string number TEST: N/A

--- lanyusea/1001.c
+++ lanyusea/1001.c
@@ -1,20 +1,78 @@
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include "lib.h"
 
 #define INPUT_SIZE 6
 
-void char_num_shift(char* input, int input_size, int shift,
+result_t char_num_shift_left(char* input, int input_size, int shift,
         char* result, int* result_size)
 {
-
+    *result_size = input_size + shift;
+    result = (char*) calloc(*result_size, sizeof(char));
+    if (result == NULL)
+    {
+        return 1;
+    }
+    memcpy(result, input, input_size);
+    return 0;
 }
 
-void char_num_sum(char* input_1, int input_1_size,
+result_t char_num_shift_right(char* input, int input_size, int shift,
+        char* result, int* result_size)
+{
+    if (input_size < shift)
+    {
+        return 1;
+    }
+    *result_size = input_size - shift;
+    result = (char*) calloc(*result_size, sizeof(char));
+    if (result == NULL)
+    {
+        return 1;
+    }
+    memcpy(result, input, *result_size);
+    return 0;
+}
+
+result_t char_num_sum(char* input_1, int input_1_size,
         char* input_2, int input_2_size,
         char* result, int* result_size)
 {
+    *result_size = (input_1_size > input_2_size)?input_1_size + 1:input_2_size + 1;
+    result = (char*) calloc(*result_size, sizeof(char));
+    if (result == NULL)
+    {
+        return 1;
+    }
 
+    int i, j;
+    int carry = 0;
+    for (i = input_1_size - 1, j = input_2_size - 1; i >= 0 && j >= 0; i --, j--)
+    {
+        result[i+1] = INT_TO_CHAR((carry + CHAR_TO_INT(input_1[i]) + CHAR_TO_INT(input_2[i]) % 10 ));
+        carry = (carry + CHAR_TO_INT(input_1[i]) + CHAR_TO_INT(input_2[i])) / 10;
+    }
+    for (;i >= 0; i--)
+    {
+        result[i+1] = INT_TO_CHAR((carry + CHAR_TO_INT(input_1[i])) % 10);
+        carry = (carry + CHAR_TO_INT(input_1[i]))/ 10;
+    }
+    for (;j >= 0; i--)
+    {
+        result[j+1] = INT_TO_CHAR((carry + CHAR_TO_INT(input_1[j])) % 10);
+        carry = (carry + CHAR_TO_INT(input_1[j]))/ 10;
+    }
+    result [i] = INT_TO_CHAR(carry);
+
+    if (result[0] == '0')
+    {
+        /* cause mem leak, but make it fast */
+        result += 1;
+        *result_size -= 1;
+    }
+    return 0;
 }
 
 void char_multiple (char* input_1, int input_1_size,
--- lanyusea/lib.h
+++ lanyusea/lib.h
@@ -1,3 +1,7 @@
 #define CHAR_TO_INT(a) \
     ((a)-'0')
 
+#define INT_TO_CHAR(a) \
+    ((a)+'0')
+
+#define result_t uint8_t
